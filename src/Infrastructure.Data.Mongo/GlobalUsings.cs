574ac53f38fefaeb8e9ec73c861146329b8981e3
Felipe Ricci <scan.ricci@voeazul.com.br>
2025-12-18 23:38:14 -0300

    Removal of unused using statements and initial setup of unit tests

--- src/Infrastructure.Data.Mongo/GlobalUsings.cs
+++ src/Infrastructure.Data.Mongo/GlobalUsings.cs
@@ -1,6 +1,4 @@
 ﻿global using CrossCutting.Configuration;
-global using Domain.Interfaces.Example.Entity;
-global using Infrastructure.Data.Mongo.Repositories.v1.Base;
 global using MongoDB.Bson.Serialization;
 global using MongoDB.Driver;
 global using System.Diagnostics.CodeAnalysis;
---
+++ src/Testes.Unit/Domain/BodyMetricsAnalyzer/BodyCompositionAnalyzerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Testes.Unit.Domain.BodyMetricsAnalyzer;
+
+public class BodyCompositionAnalyzerTests
+{
+    [Fact]
+    public void Should_Pass()
+    {
+        var result = true;
+
+        result.Should().BeTrue();
+    }
+}
